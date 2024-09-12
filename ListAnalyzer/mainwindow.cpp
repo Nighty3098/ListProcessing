09e4b41e2313a266b500d7694518340660cce50e
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-09-12 16:38:32 +0700

    Update mainwindow.cpp

--- ListAnalyzer/mainwindow.cpp
+++ ListAnalyzer/mainwindow.cpp
@@ -29,11 +29,42 @@ void MainWindow::on_saveDataBtn_clicked() {
 }
 
 void MainWindow::on_startProcessBtn_clicked() {
-    int mode = ui->actionBox->currentIndex();
-    dataProcessor->processFile(filePath, mode);
-    ui->tabWidget->setCurrentIndex(2);
+    QString dataPath = ui->filePathLbl->text();
+    if (!dataPath.isEmpty()) {
+        int mode = ui->actionBox->currentIndex();
+        dataProcessor->processFile(filePath, mode);
+        ui->tabWidget->setCurrentIndex(2);
+    }
+    else {
+        QMessageBox::information(this, "!!!", "Сначала откройте файл");
+    }
 }
 
 void MainWindow::updateOutput(const QString &output) {
     ui->outputText->setText(output);
+}
+
+void MainWindow::on_addDataBtn_clicked() {
+    QString dataPath = ui->filePathLbl->text();
+
+    if (!dataPath.isEmpty()) {
+        QFile file(dataPath);
+
+        if (!file.open(QIODevice::Append | QIODevice::Text)) {
+            qWarning() << "Cannot open file for writing:" << dataPath;
+            return;
+        }
+
+        QTextStream out(&file);
+        out << ui->objNameData->text() << " "
+            << ui->objPosData->text() << " "
+            << ui->objTypeData->text() << " "
+            << ui->objCTData->text() << "\n";
+        file.close();
+
+        QMessageBox::information(this, "!!!", "Данные записаны");
+    }
+    else {
+        QMessageBox::information(this, "!!!", "Сначала откройте файл");
+    }
 }
